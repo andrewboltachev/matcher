e9dbeb2a2349862578a0f519ea34ab444c7d1d31
Andrei Boltachev <andrewboltachev@gmail.com>
2025-02-01 20:36:26 +0300

    csrf fixed

--- src/Logicore/Ex2.hs
+++ src/Logicore/Ex2.hs
@@ -10,19 +10,27 @@ import Control.Exception
 import qualified Data.ByteString.Lazy as LBS
 import Data.Foldable
 import Language.JavaScript.Inline
+import Language.JavaScript.Inline.Core
 
 --js1 :: Int -> IO LBS.ByteString
-js1 foo = do
-  _session <- newSession defaultConfig
-  eval @LBS.ByteString
+js1 _session foo = do
+  eval @EncodedJSON
     _session
     [js|
       console.log("aha");
-      return `${$foo.a + 2}`;
+      $foo.a += 1;
+      $foo.b = `${$foo.a + 2}`;
+      $foo.c = new Date();
+      return $foo;
     |]
 
 
-m1 :: IO ()
-m1 = do
-  v <- js1 (EncodedJSON "{\"a\": 1}")
-  print v
+j1 :: IO ()
+j1 = do
+  _session <- newSession defaultConfig
+  v <- js1 _session (EncodedJSON "{\"a\": 1}")
+  print $ unEncodedJSON v
+  --v <- js1 _session (EncodedJSON "{\"a\": 2}")
+  --print v
+  --v <- js1 _session (EncodedJSON "{\"a\": 3}")
+  --print v
